fc6a3aedc374606d9e475a77e7f5e04691985b14
Filipe Lima <filipe@xpenditure.com>
2018-02-18 18:57:25 +0100

    fixing enviroment file

--- source/src/environments/environment.prod.ts
+++ source/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  acerolaApiUrl: 'http://grape.westus2.cloudapp.azure.com:8000/api/',
+  mangaApiUrl: 'http://grape.westus2.cloudapp.azure.com:8800/api/'
 };
